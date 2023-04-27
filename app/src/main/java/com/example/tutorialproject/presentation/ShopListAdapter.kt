0537a8772a0248c5ee1ee13ec7aa2a9176f83d0f
mishilda <pavlovamar96@yandex.ru>
2023-04-27 12:12:47 +0300

    chapter 4

---
+++ app/src/main/java/com/example/tutorialproject/presentation/ShopListAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.tutorialproject.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.tutorialproject.R
+import com.example.tutorialproject.domain.ShopItem
+
+class ShopListAdapter : ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCallback()) {
+
+    var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
+    var onShopItemClickListener: ((ShopItem) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val layout = when(viewType) {
+            VIEW_TYPE_ENABLED ->  R.layout.item_shop_enabled
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            else -> throw RuntimeException("Unknown view type: $viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(
+            layout,
+            parent,
+            false
+        )
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = getItem(position)
+        holder.tvName.text = shopItem.name
+        holder.tvCount.text = shopItem.count.toString()
+        holder.itemView.setOnLongClickListener {
+            onShopItemLongClickListener?.invoke(shopItem)
+            true
+        }
+        holder.itemView.setOnClickListener {
+            onShopItemClickListener?.invoke(shopItem)
+        }
+    }
+
+
+
+    override fun getItemViewType(position: Int): Int {
+        val shopItem = getItem(position)
+        return if (shopItem.enabled)
+            VIEW_TYPE_ENABLED
+        else
+            VIEW_TYPE_DISABLED
+    }
+
+    companion object {
+        const val VIEW_TYPE_ENABLED = 1
+        const val VIEW_TYPE_DISABLED = 0
+
+        const val MAX_POOL_SIZE = 15
+    }
+}
